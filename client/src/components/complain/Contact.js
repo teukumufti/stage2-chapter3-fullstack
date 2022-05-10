893a1b1f2fedec6fb4c4fb7c71d012f8d35784e3
teukumufti <teukumufti9770@gmail.com>
2022-05-10 12:27:13 +0700

    update

--- client/src/components/complain/Contact.js
+++ client/src/components/complain/Contact.js
@@ -7,11 +7,21 @@ const Contact = ({ dataContact, clickContact, contact }) => {
       {dataContact.length !== 0 && (
         <div className="d-grid mt-4">
           {dataContact.map((item) => (
-            <button key={item.id} onClick={() => clickContact(item)} className="btn-dark d-flex align-items-center p-3 rounded mb-2">
-              <img className="rounded-circle icon-profile-circle" src={Image} alt="Profile" />
+            <button
+              key={item.id}
+              onClick={() => clickContact(item)}
+              className="btn-dark d-flex align-items-center p-3 rounded mb-2"
+            >
+              <img
+                className="rounded-circle icon-profile-circle"
+                src={Image}
+                alt="Profile"
+              />
               <div className="ms-3 text-start">
                 <span className="d-block text-light fw-bold">{item.name}</span>
-                <span className="d-block text-small  text-var-gray">{item.message}</span>
+                <span className="d-block text-small  text-var-gray">
+                  {item.message}
+                </span>
               </div>
             </button>
           ))}
